db6a564fd8f755371ce14111ebe95b3c15c106ac
Husniye <husniyesakmak.91@gmail.com>
2020-12-12 15:52:43 +0100

    project modified

--- guessNumber/main.js
+++ guessNumber/main.js
@@ -1,12 +1,12 @@
-let suspectWagon = Math.floor((Math.random() *10)+1);
-alert(suspectWagon);
+let suspectWagon = Math.floor((Math.random() * 10) + 1);
 for (let selectionRepeatNumber = 1; selectionRepeatNumber <= 4; selectionRepeatNumber++) {
     let chosenWagon = prompt("Chose Wagon");
     if (selectionRepeatNumber == 4 && chosenWagon != suspectWagon) {
         alert(`You can not find Suspect. The suspect is in wagon number ${ suspectWagon } `)
     } else {
         if (chosenWagon == suspectWagon) {
-            alert(`You can find suspect. Yor scorre 100-25*${selectionRepeatNumber-1} `);
+            let puan = 100 - 25 * (selectionRepeatNumber - 1)
+            alert(`You can find suspect. Yor scorre ${puan} `);
             break;
         } else if (chosenWagon < suspectWagon) {
             alert("FORWARD TO THE BACK WAGON")
