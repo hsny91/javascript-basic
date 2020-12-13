d21f5e1c2f0daa340148674cf1bbedbd24073bd5
Husniye <husniyesakmak.91@gmail.com>
2020-12-13 19:30:47 +0100

    main.js file has been modified

---
+++ stringOpreration/main.js
@@ -0,0 +1,7 @@
+/** 1)  string olarak verilen bir isim ve soyismin sadece bas harflerini Uppercase a cevirin.
+    3)  0 dan 100 e kadar olan prime (asal sayilari) yazdirin (Asal sayilar: sadece kendisine ve 1 e bolunebilen sayilar)
+         bulunmasi gereken sonuc: 1, 2, 3, 5, 7, 11 ....
+*/
+let nameSurname= "asik veysel";
+let newNameSurname=nameSurname.charAt(0).toUpperCase();
+console.log(newNameSurname);
