082a8792cba1faffd362eeb1ad455207b241b323
Husniye <husniyesakmak.91@gmail.com>
2020-12-13 20:06:23 +0100

    project has been modified

--- stringOpreration/main.js
+++ stringOpreration/main.js
@@ -1,7 +1,22 @@
 /** 1)  string olarak verilen bir isim ve soyismin sadece bas harflerini Uppercase a cevirin.
     3)  0 dan 100 e kadar olan prime (asal sayilari) yazdirin (Asal sayilar: sadece kendisine ve 1 e bolunebilen sayilar)
          bulunmasi gereken sonuc: 1, 2, 3, 5, 7, 11 ....
+    5)  Asagidaki sekli programlama yapilarini kullanarak cizdiriniz.
+​
+          *
+          **
+          ***
+          ****
+          *****
+          ******
 */
-let nameSurname= "asik veysel";
-let newNameSurname=nameSurname.charAt(0).toUpperCase();
-console.log(newNameSurname);
+
+// String olarak verilen bir isim ve soyismin sadece bas harflerini Uppercase'e cevirme.
+const nameSurname = "asik veysel";
+let nameSurnameList = nameSurname.split(" ");
+for (let indexOfList = 0; indexOfList < nameSurnameList.length; indexOfList++) {
+    nameSurnameList[indexOfList] = nameSurnameList[indexOfList].charAt(0).toUpperCase() + nameSurnameList[indexOfList].slice(1);
+}
+let upperFirstLetter = nameSurnameList.join(" ");
+console.log(upperFirstLetter);
+
