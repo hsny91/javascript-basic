663cb6bfbfbf3613cc5f7c6568110531b0bd489d
Husniye <husniyesakmak.91@gmail.com>
2020-12-16 15:42:28 +0100

    project modified

--- stringOpreration/main.js
+++ stringOpreration/main.js
@@ -2,7 +2,7 @@
  *  2)  0 dan 100 e kadar olan sayilari, bir satirda 10 sayi ve her sayinin arasinda virgul olacak sekilde yazdiriniz (10x10 luk bir     *  kare seklinde)
  *  3)  0 dan 100 e kadar olan prime (asal sayilari) yazdirin (Asal sayilar: sadece kendisine ve 1 e bolunebilen sayilar)
  *   bulunmasi gereken sonuc: 1, 2, 3, 5, 7, 11 ....
- *  4)
+ *  4)Kucuk, buyuk, ozel karakterler ve sayilardan olusan 8 ile 16 (hane) uzunluklari arasinda guvenli 50 adet sifre olusturunuz. 
  *  5)  Asagidaki sekli programlama yapilarini kullanarak cizdiriniz.
 ​
           *
@@ -25,14 +25,7 @@ console.log(upperFirstLetter);
 // 10X10 Matris
 let matris;
 
-// yildiz olusturma 
-let starList = [];
-const MAX_STAR=6;
-const YILDIZ="*";
-for (let startIndex = 0; startIndex < MAX_STAR; startIndex++) {
-    starList.push(YILDIZ)
-    console.log(starList.join(""))
-}
+
 
 // Asal sayilari bulma
 const MAX_BOUNDARY = 100;
@@ -48,4 +41,12 @@ for (let primeNumber = 2; primeNumber < MAX_BOUNDARY; primeNumber++) {
         console.log(primeNumber);
     }
 }
-console.log("ahmet \n ayse")
+// password olusturma 
+// yildiz olusturma 
+let starList = [];
+const MAX_STAR=6;
+const YILDIZ="*";
+for (let startIndex = 0; startIndex < MAX_STAR; startIndex++) {
+    starList.push(YILDIZ)
+    console.log(starList.join(""))
+}
