079af7b8489f2118340bdd9ad3f54c5c0fa819aa
Husniye <husniyesakmak.91@gmail.com>
2020-12-20 19:35:03 +0100

    project modified

--- findAnimal/main.js
+++ findAnimal/main.js
@@ -1,28 +1,29 @@
-const AnimalList = ["cow", "sheep", "chicken", "pig", "cow", "cow", "pig", "sheep", "cow", "sheep", "cow", "cow", "chicken"];
-const chickenLeg = 2;
-const pigLeg = 4;
-const sheepLeg = 4;
-const cowLeg = 4;
+const animalList = ["cow", "sheep", "chicken", "pig", "cow", "cow", "pig", "sheep", "cow", "sheep", "cow", "cow", "chicken"];
+const CHICKEN_LEG = 2;
+const PIG_LEG = 4;
+const SHEEP_LEG = 4;
+const COW_LEG = 4;
 let numberOfCow = 0,
     numberOfShepp = 0,
     numberOfChicken = 0,
     numberOfPig = 0;
 
-for (i = 0; i < AnimalList.length; i++) {
-    if (AnimalList[i].includes("cow")) {
+for (let index = 0; index < animalList.length; index++) {
+    let nextAnimal = animalList[index].toLowerCase();
+    if (nextAnimal.includes("cow")) {
         numberOfCow++
-    }
-    else if (AnimalList[i].includes("chicken")) {
+    } else if (nextAnimal.includes("chicken")) {
         numberOfChicken++
-    }
-    else if (AnimalList[i].includes("pig")) {
+    } else if (nextAnimal.includes("pig")) {
         numberOfPig++
-    }
-    else if (AnimalList[i].includes("sheep")) {
+    } else if (nextAnimal.includes("sheep")) {
         numberOfShepp++
     }
-
 }
-let helalBacakSayisi = numberOfCow * cowLeg + numberOfShepp * sheepLeg + numberOfChicken * chickenLeg;
-console.log(`Helal bacak sayisi: ${helalBacakSayisi}`);
 
+let numberOfHalalLeg = numberOfCow * COW_LEG + numberOfShepp * SHEEP_LEG + numberOfChicken * CHICKEN_LEG;
+let totalLeg = numberOfChicken * CHICKEN_LEG + numberOfCow * COW_LEG + numberOfPig * PIG_LEG + numberOfShepp * SHEEP_LEG;
+let kellePacaLeg = numberOfShepp * SHEEP_LEG + numberOfCow * COW_LEG;
+console.log(`Number of Halal Legs: ${numberOfHalalLeg}`);
+console.log(`Number of Total Legs: ${totalLeg}`);
+console.log(`Number of Kelle Paca Legs: ${kellePacaLeg}`);
