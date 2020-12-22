6c16f117bf2eff5e52a6cd662c514233074011f1
Husniye <husniyesakmak.91@gmail.com>
2020-12-22 12:48:46 +0100

    project modified

--- Two dimensional arrays/main.js
+++ Two dimensional arrays/main.js
@@ -1,37 +1,69 @@
 const notes = [
-    ["Hans", 80, 90, 10, 50], 
-    ["Ulrich", 94, 36, 76, 74], 
-    ["Olaf", 30, 60, 34, 78], 
+    ["Hans", 80, 90, 10, 50],
+    ["Ulrich", 94, 36, 76, 74],
+    ["Olaf", 30, 60, 34, 78],
     ["Otto", 60, 60, 78, 23],
     ["Tina", 71, 45, 89, 100],
     ["Rosalina", 40, 56, 82, 40],
     ["Urs", 49, 45, 56, 67],
     ["Monika", 49, 42, 16, 62]
-];/**
- * 
+];
+
+/**
  * @param {*} pNoteList 
  * @param {*} pIndex 
  * 
- * finding the Avarage note function
+ * finding the Average note function
  */
-function findLessonAverage(pNoteList,pIndex){
-    let toplam=0; 
-    pNoteList.map(note=>toplam+=note[pIndex]);
-    let average=toplam/pNoteList.length;
-    console.log(average)
+function findLessonAverage(pNoteList, pIndex) {
+    let toplam = 0;
+    pNoteList.map(note => toplam += note[pIndex]);
+    let average = toplam / pNoteList.length;
     return average;
 }
+
+/**
+ *The function that finds students who pass 70 points
+
+ * @param {*} pNotesList 
+ */
+function passBoundaryNote(pNotesList, pboundary, pIndex) {
+    let hardworkingStudent = [];
+    pNotesList.map(note => {
+        if (note[pIndex] > pboundary)
+            hardworkingStudent.push(note);
+    })
+    return hardworkingStudent;
+}
+
 // Math Average
-findLessonAverage(notes,1);
+let mathAverage=findLessonAverage(notes, 1);
+console.log(`Math Average: ${mathAverage}`);
 
 // German Avarage
-findLessonAverage(notes,2);
+let germanAverage=findLessonAverage(notes, 2);
+console.log(`German Average: ${germanAverage}`);
 
 //English Average
-findLessonAverage(notes,3);
+let englishAverage=findLessonAverage(notes, 3);
+console.log(`English Average: ${englishAverage}`);
 
 //Geography Average
-findLessonAverage(notes,4);
+let geographyAverage=findLessonAverage(notes, 4);
+console.log(`Geography Average: ${geographyAverage}`);
 
+// Students who passed 70 in mathematics
+let mathExpert=passBoundaryNote(notes, 70, 1);
+console.log(`Students with more than 70 grades in Math: ${mathExpert}`)
 
+// Students who passed 70 in German
+let germanExpert=passBoundaryNote(notes, 70, 2);
+console.log(`Students with more than 70 grades in German: ${germanExpert}`)
 
+// Students who passed 70 in English
+let englishExpert=passBoundaryNote(notes, 70, 3);
+console.log(`Students with more than 70 grades in English: ${englishExpert}`)
+
+// Students who passed 70 in Geography
+let geographyExpert=passBoundaryNote(notes, 70, 4);
+console.log(`Students with more than 70 grades in Geography: ${geographyExpert}`)
