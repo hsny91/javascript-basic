46be7fa63f9be63dcb2b087e5d300a48f3634f6e
Husniye <husniyesakmak.91@gmail.com>
2020-12-22 19:59:37 +0100

    project modified

--- arrayMethods/main.js
+++ arrayMethods/main.js
@@ -8,6 +8,23 @@ const carParts = ["crk725", "karb789", "phyt234", "mech256", "tesl345", "cabr234
 
 // convert to upperCase
 function convertToUpperCase(pPartList) {
-    let upperCasePartList = pPartList.map(part => part.ToUpperCase());
+    let upperCasePartList = pPartList.map(part => part.toUpperCase());
+    console.log(upperCasePartList);
     return upperCasePartList;
 }
+//Numbers will be subtracted from part names
+function subtractedNumbers(pPartList){
+    let subtractedNumberList=pPartList.map(part=>part.replace(/[0-9]/g, ''));
+    console.log(subtractedNumberList);
+    return subtractedNumberList;
+
+}
+//conver to reverse
+function convertToReverse(pPartList) {
+    let reversePartList = pPartList.map(part =>part.split("").reverse().join(""));
+    console.log(reversePartList);
+    return reversePartList;
+}
+convertToUpperCase(carParts);
+convertToReverse(carParts);
+subtractedNumbers(carParts);
