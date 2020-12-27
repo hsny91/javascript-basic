7494fd1099f625071e7021a589367e778d59389e
Husniye <husniyesakmak.91@gmail.com>
2020-12-27 17:18:53 +0100

    project modified

--- functions/main.js
+++ functions/main.js
@@ -1,10 +1,4 @@
-/**
- * 1. Parca isimleri büyük harflere cevrilecektir.
-2. Parca isimlerinden sayilar cikartilacaktir.
-3. Parca isimleri tersine cevrilecektir.
-4. Her parcanin basina KEREMAG_ eklenecektir.
-5. Her parcanin sonuna ise parcanin sisteme girildigi tarih eklenecektir. (Date nesnesini kullanarak tarih ve saat konulmasi yeterlidi
- */
+
 let partList=["Radyatör23", "Sanziman34", "Filtre3", "Direksiyon5", "Kaporta2", "ayna09"];
 
 /**
