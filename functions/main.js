ec58379881168b4ef3883b5247cc7d32127af667
Husniye <husniyesakmak.91@gmail.com>
2020-12-27 16:57:55 +0100

    project created

---
+++ functions/main.js
@@ -0,0 +1,7 @@
+/**
+ * 1. Parca isimleri büyük harflere cevrilecektir.
+2. Parca isimlerinden sayilar cikartilacaktir.
+3. Parca isimleri tersine cevrilecektir.
+4. Her parcanin basina KEREMAG_ eklenecektir.
+5. Her parcanin sonuna ise parcanin sisteme girildigi tarih eklenecektir. (Date nesnesini kullanarak tarih ve saat konulmasi yeterlidi
+ */
