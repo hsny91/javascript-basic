30cf87a3be047e17f44a4e2889e0575df7d1112b
Husniye <husniyesakmak.91@gmail.com>
2020-12-27 17:16:25 +0100

    project modified

--- functions/main.js
+++ functions/main.js
@@ -15,17 +15,36 @@ function convertToUpperCase(pPartList){
 let upperCasePartList=pPartList.map(part=>part.toUpperCase())
 return upperCasePartList;
 }
+
+/**
+ * throw number
+ * @param {*} pPartList 
+ */
 function throwNumber(pPartList){
     let throwNumberList=pPartList.map(part=>part.replace(/[0-9]/g,""))
     return throwNumberList;
 }
+
+/**
+ * reverse arrays element
+ * @param {*} pPartList 
+ */
 function reverseList(pPartList){
     let reverseList=pPartList.map(part=>part.split("").reverse().join(""))
     return reverseList;
+}
+/**
+ * add array elemets Keremag
+ * @param {*} pPartList 
+ */
+function addName(pPartList){
+    let addNameList=pPartList.map(part=>"KEREMAG_"+part)
+    return addNameList;
 }
 
 let upperCasePartList=convertToUpperCase(partList);
 let throwNumberList=throwNumber(upperCasePartList);
 let reverseList=reverseList(throwNumberList);
+let addNammeList=addName(addNammeList);
 
 console.log(reverseList(throwNumberList));
