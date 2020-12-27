4158368a7aae5d011f471d878c65006a5be1f9da
Husniye <husniyesakmak.91@gmail.com>
2020-12-27 17:00:12 +0100

    project modified

--- functions/main.js
+++ functions/main.js
@@ -5,3 +5,4 @@
 4. Her parcanin basina KEREMAG_ eklenecektir.
 5. Her parcanin sonuna ise parcanin sisteme girildigi tarih eklenecektir. (Date nesnesini kullanarak tarih ve saat konulmasi yeterlidi
  */
+let partList=["Radyatör23", "Sanziman34", "Filtre3", "Direksiyon5", "Kaporta2", "ayna09"];
