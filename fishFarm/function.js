4939fbb369fc503bceebe1563c1a9e193d25392c
Husniye <husniyesakmak.91@gmail.com>
2021-01-04 13:08:22 +0100

    fishFarm file was created

---
+++ fishFarm/function.js
@@ -0,0 +1,16 @@
+/*Cabbar, Kerem ve Sinan ortaklasa balik ciftligi projesine girerler. Cabbar ve Keremin referansi ile Sinan da bizlerden bir program istemektedir.
+Uc kafadar bizden ciftlikteki baliklarin satisi icin bir online sistem istemektedirler. Online satis sistemi asagidaki sorulara cevap verebilmelidir.
+
+1) Stok miktari 500 kg uzerinde olan baliklarin isimleri nelerdir?
+2) Fiyat araligi 9Fr. ile 12 Fr. arasindaki baliklar hangileridir?
+3) Sadece Bern'de ve kis sezonu satilan baliklar hangileridir?
+4) Son kullanma tarihlerine gore baliklari siralayiniz. (Son kullanma tarihi yaklasan baliklar once gosterilmelidir)
+5) Avrupa Birligi'nden (AB) gelen ve fiyati 10Fr dan dusuk olan baliklari alfabetik siraya gore siralayiniz.
+6) Toplam balik stoku ne kadardir?
+7) En pahali olan balik hangisidir? 
+8) En uzun sureli durabilen baliklar hangi ulkeden gelmektedir?
+9) Kis ve sonbahar sezonu icin swiss romande region'da satilan baliklarin ortalama fiyati nedir?
+10) Ticino Kantonu icin stokta toplam ne kadar balik mevcuttur?
+11) Yazlik sezonda cikan ve AB disindan gelen ve de ZH'de satilan baliklarin ortalama gramajini bulunuz?*/
+
+
