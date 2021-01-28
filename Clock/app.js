614a93f077fca5dbb4e30edc9fe9b754e10576c2
Husniye <husniyesakmak.91@gmail.com>
2021-01-28 19:56:07 +0100

    project was modified

--- Clock/app.js
+++ Clock/app.js
@@ -1,14 +1,5 @@
 
-  let clock = (pLand,pOrt,pID) => {
-    var d = new Date();
 
-   var datem=d.toLocaleTimeString(pLand, { timeZone: pOrt })
-    
-  
-    let time = `${datem}`;
-    document.getElementById(`clock${pID}`).innerText = time;
-  
-};
 
 
 
--- Clock/function.js
+++ Clock/function.js
@@ -1,5 +1,14 @@
 
+  let clock = (pLand,pOrt,pID) => {
+    var d = new Date();
 
+   var datem=d.toLocaleTimeString(pLand, { timeZone: pOrt })
+    
+  
+    let time = `${datem}`;
+    document.getElementById(`clock${pID}`).innerText = time;
+  
+};
 
 
   
