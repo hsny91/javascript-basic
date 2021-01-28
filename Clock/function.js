c3a6136e0d146dcb7b9be090bd4c50f1bf7cec5a
Husniye <husniyesakmak.91@gmail.com>
2021-01-28 19:58:57 +0100

    project was modified

--- Clock/function.js
+++ Clock/function.js
@@ -1,20 +1,22 @@
-
-  let clock = (pLand,pOrt,pID) => {
+let clock = (pLand, pOrt, pID) => {
     var d = new Date();
-
-   var datem=d.toLocaleTimeString(pLand, { timeZone: pOrt })
-    
-  
+    var datem = d.toLocaleTimeString(pLand, {
+        timeZone: pOrt
+    })
     let time = `${datem}`;
     document.getElementById(`clock${pID}`).innerText = time;
-  
 };
 
-let start= ()=>{
-    setInterval(function() {clock('en-US','America/New_York',0)}, 1000);
+let start = () => {
+    setInterval(function () {
+        clock('en-US', 'America/New_York', 0)
+    }, 1000);
 
-    setInterval(function() {clock('en-NZ','Pacific/Chatham',1)}, 1000);
-    
-    setInterval(function() {clock('en-GB','Europe/London',2)}, 1000);
+    setInterval(function () {
+        clock('en-NZ', 'Pacific/Chatham', 1)
+    }, 1000);
+
+    setInterval(function () {
+        clock('en-GB', 'Europe/London', 2)
+    }, 1000);
 }
-  
