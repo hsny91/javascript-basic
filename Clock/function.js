dc12471982951709def841b5110a0dc9da551fcb
Husniye <husniyesakmak.91@gmail.com>
2021-01-29 23:51:02 +0100

    project was modified

--- Clock/function.js
+++ Clock/function.js
@@ -1,3 +1,16 @@
+function start() {
+    setInterval(function () {
+        clock('en-US', 'America/New_York', 0)
+    }, 1000);
+
+    setInterval(function () {
+        clock('en-NZ', 'Pacific/Chatham', 1)
+    }, 1000);
+
+    setInterval(function () {
+        clock('en-GB', 'Europe/London', 2)
+    }, 1000);
+}
 
 function start() {
     setInterval(function () {
