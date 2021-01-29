c94d68bfc6fd8d430b02e29860f412fdc8a8fe6a
Husniye <husniyesakmak.91@gmail.com>
2021-01-29 23:50:22 +0100

    project was modified

--- Clock/function.js
+++ Clock/function.js
@@ -7,3 +7,16 @@ let clock = (pLand, pOrt, pID) => {
     document.getElementById(`clock${pID}`).innerText = time;
 };
 
+function start() {
+    setInterval(function () {
+        clock('en-US', 'America/New_York', 0)
+    }, 1000);
+
+    setInterval(function () {
+        clock('en-NZ', 'Pacific/Chatham', 1)
+    }, 1000);
+
+    setInterval(function () {
+        clock('en-GB', 'Europe/London', 2)
+    }, 1000);
+}
