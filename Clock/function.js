960b03722b95c836a966f00ae67c29b7b152ce12
Husniye <husniyesakmak.91@gmail.com>
2021-01-29 23:50:51 +0100

    project was modified

--- Clock/function.js
+++ Clock/function.js
@@ -1,11 +1,3 @@
-let clock = (pLand, pOrt, pID) => {
-    var d = new Date();
-    var datem = d.toLocaleTimeString(pLand, {
-        timeZone: pOrt
-    })
-    let time = `${datem}`;
-    document.getElementById(`clock${pID}`).innerText = time;
-};
 
 function start() {
     setInterval(function () {
