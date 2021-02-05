b3e2c8974588d069eaf78e04a9e27e78fa04176a
Husniye <husniyesakmak.91@gmail.com>
2021-02-05 16:24:49 +0100

    project was modified

--- URLAPI/urlApi.js
+++ URLAPI/urlApi.js
@@ -1,6 +1,8 @@
 let address = new URL("https://developer.mozilla.org/en-US/docs/Web/API/URL_API");
 let host = address.host;
 let path = address.pathname;
+console.log(host)
+console.log(path)
 
 
 let myUsername = "someguy";
