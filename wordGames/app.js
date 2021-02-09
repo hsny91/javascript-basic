a0a7b7fa865804d9d6506b321653d47b44d9ecff
Husniye <husniyesakmak.91@gmail.com>
2021-02-09 18:30:57 +0100

    project was modifief

--- wordGames/app.js
+++ wordGames/app.js
@@ -1,1 +1,31 @@
-genRandom(['HUS', 'NI', 'YE'])
+// let words = [{
+//     name: "BILGISAYAR",
+//     syllables: ["BIL", "GI", "SA", "YAR"]
+//   },
+//   {
+//     name: "DEFTER",
+//     syllables: ["DEF", "TER"]
+//   }
+let wordList = [{
+        word: ['HUS', 'NI', 'YE']
+    },
+    {
+        word: ['DEF', 'TER',]
+    },
+    {
+        word: ['DE', 'NE', 'ME']
+    },
+    {
+        word: ['A', 'YAK','KA','BI']
+    },
+    {
+        word: ['MEH', 'MET']
+    },
+
+]
+
+let wordArrayLenght=wordList.length;
+let index=Math.floor(Math.random() * wordArrayLenght); 
+
+
+genRandom(wordList[index].word)
