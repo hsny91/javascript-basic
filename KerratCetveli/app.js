8f2b4cae326ff7a54cf911527f7e3c954b0ab4cb
Husniye <husniyesakmak.91@gmail.com>
2021-02-16 14:52:51 +0100

    project was modified

--- KerratCetveli/app.js
+++ KerratCetveli/app.js
@@ -1,4 +1,12 @@
-
+/**
+ * Okul Kayit Sistemi
+ * 1.Ekranin sol tarafinda bir form olusturulacak,
+ * 2.Forma girilen bilgiler kategorisine göre kontrol edilecek (rakam veya alfabetik kontrol),
+ * 3.Form'a bir ekle butuno koyulacak,
+ * 4.Butona tiklandiginda Form bilgileri bir arraye atilacak, 
+ * 5.Array'de tutulan bilgiler ekranin sag tarafina yazdirilacak,
+ * 6.Listenin sonuna toplam ögrenci sayisi yazdirilacak.
+ */
 const mainElement = document.querySelector("#app");
 let counter=0;
 
@@ -95,6 +103,7 @@ mainElement.addEventListener("click", (event) => {
         playerList.push({
             playerName: playerNameArea.value,
         })
+        console.log(playerList)
         createStartUI();
     }
 })
