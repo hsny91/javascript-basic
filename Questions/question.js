b8ba7f0e057161f7965eae82abb8fdbdc0410b66
Husniye <husniyesakmak.91@gmail.com>
2021-02-26 14:12:51 +0100

    project was modified

--- Questions/question.js
+++ Questions/question.js
@@ -1,3 +1,9 @@
-let printOnScreen=()=>{
-    
-}
+let nameList=["ayse","ali","ahmet","mehmet","elif"]
+nameList.map((name)=>{
+document.querySelector("#app").innerHTML=name;
+})
+
+ let printOnScreen=(pColor)=>{  
+    document.querySelector("#color").innerHTML=pColor;
+ }
+ printOnScreen("RED")
