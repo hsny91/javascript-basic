6d8f91301b04ce6d8040982f5b32a2e025fa637a
Husniye <husniyesakmak.91@gmail.com>
2021-02-26 14:37:03 +0100

    project was modified

--- Questions/question.js
+++ Questions/question.js
@@ -1,9 +1,12 @@
-let nameList=["ayse","ali","ahmet","mehmet","elif"]
-nameList.map((name)=>{
-document.querySelector("#app").innerHTML=name;
-})
+// let nameList=["ayse","ali","ahmet","mehmet","elif"]
+// nameList.map((name)=>{
+// document.querySelector("#app").innerHTML=name;
+// })
 
- let printOnScreen=(pColor)=>{  
-    document.querySelector("#color").innerHTML=pColor;
- }
- printOnScreen("RED")
+//  let printOnScreen=(pColor)=>{  
+//     document.querySelector(".color").innerHTML=pColor;
+//  }
+//  printOnScreen("RED")
+
+document.getElementById("myBtn").addEventListener("click", displayDate());
+
