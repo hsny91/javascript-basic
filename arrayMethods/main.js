431e400612b88731d4cba09f0698e1f6af31eef4
Husniye <husniyesakmak.91@gmail.com>
2021-03-03 22:57:29 +0100

    project was modified

--- arrayMethods/main.js
+++ arrayMethods/main.js
@@ -1,8 +1,4 @@
-//1. Parca isimleri büyük harflere cevrilecektir.
-//2. Parca isimlerinden sayilar cikartilacaktir.
-//3. Parca isimleri tersine cevrilecektir.
-//4. Her parcanin basina KEREMAG_ eklenecektir.
-//5. Her parcanin sonuna ise parcanin sisteme girildigi tarih eklenecektir. (Date nesnesini kullanarak tarih ve saat konulmasi yeterlidir)
+
 
 const carParts = ["crk725", "karb789", "phyt234", "mech256", "tesl345", "cabr234"]
 
